56aaa79f230c2dc3f4183872bcb9b9098333d6bf
Nevo Saporta <nevosap@gmail.com>
2020-03-30 14:03:03 +0300

    Created Home fragment & its layout

---
+++ app/src/main/java/com/nevosap/listy/home/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.nevosap.listy.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nevosap.listy.R
+
+class HomeFragment:Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_home,container,false)
+        return view
+    }
+}
