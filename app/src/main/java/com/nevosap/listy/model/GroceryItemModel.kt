af49e2345126490cc32a2a115f13a716a1d64315
Nevo Saporta <nevosap@gmail.com>
2020-04-04 16:34:14 +0300

    Make models parcelable

--- app/src/main/java/com/nevosap/listy/model/GroceryItemModel.kt
+++ app/src/main/java/com/nevosap/listy/model/GroceryItemModel.kt
@@ -1,7 +1,11 @@
 package com.nevosap.listy.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class GroceryItemModel (
     val id:Int ,
     val name:String,
     val price:Double
-)
+):Parcelable
--- app/src/main/java/com/nevosap/listy/model/GroceryListModel.kt
+++ app/src/main/java/com/nevosap/listy/model/GroceryListModel.kt
@@ -1,10 +1,13 @@
 package com.nevosap.listy.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
 import java.util.*
 
+@Parcelize
 data class GroceryListModel (
     val id:Int,
     val name :String ="",
     val creationDate: Date,
     val items: MutableList<GroceryItemModel>
-)
+):Parcelable
