4903bf21621b81ad138c065fe3f4ccd862abb9b9
NevoSaporta <nevosap@gmail.com>
2020-04-20 15:53:54 +0300

    Declared repository's functions

--- app/src/main/java/com/nevosap/listy/repository/GroceryRepository.kt
+++ app/src/main/java/com/nevosap/listy/repository/GroceryRepository.kt
@@ -1,4 +1,12 @@
 package com.nevosap.listy.repository
 
+import com.nevosap.listy.model.GroceryItemModel
+import com.nevosap.listy.model.GroceryListModel
+
 interface GroceryRepository {
+    fun getItemsInStock(): MutableList<GroceryItemModel>
+
+    fun getAllLists():MutableList<GroceryListModel>
+
+    fun addOrUpdateList(groceryListModel: GroceryListModel) :MutableList<GroceryListModel>
 }
