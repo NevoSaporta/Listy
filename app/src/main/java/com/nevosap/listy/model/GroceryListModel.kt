f242c0266e29e0eb99984de83209b9f461c00a1b
Nevo Saporta <nevosap@gmail.com>
2020-04-22 16:12:39 +0300

    Added @Entity annotation to the item and list models

--- app/src/main/java/com/nevosap/listy/model/GroceryListModel.kt
+++ app/src/main/java/com/nevosap/listy/model/GroceryListModel.kt
@@ -1,11 +1,15 @@
 package com.nevosap.listy.model
 
 import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 import java.util.*
 
 @Parcelize
+@Entity
 data class GroceryListModel (
+    @PrimaryKey(autoGenerate = true)
     val id:Int,
     val name :String ="",
     val creationDate: Date,
