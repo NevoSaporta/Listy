22bc6c454551e38315f1bbdb02d10f8a02633bb9
Nevo Saporta <nevosap@gmail.com>
2020-04-24 16:37:39 +0300

    Created database module

--- app/src/main/java/com/nevosap/listy/database/AppDatabase.kt
+++ app/src/main/java/com/nevosap/listy/database/AppDatabase.kt
@@ -9,28 +9,7 @@ import com.nevosap.listy.model.GroceryItemModel
 @Database(entities = [GroceryItemModel::class,GroceryItemModel::class],version = 1)
 abstract class AppDatabase :RoomDatabase() {
 
-    abstract fun listDao():GroceryListsDao?
-    abstract fun itemDao():GroceryItemsDao?
+    abstract fun listDao():GroceryListsDao
+    abstract fun itemDao():GroceryItemsDao
 
-    companion object{
-        private const val DATABASE_NAME ="grocery"
-
-        private var INSTANCE :AppDatabase? =null
-
-        fun getInstance(context: Context):AppDatabase?{
-            if(INSTANCE==null){
-                INSTANCE = Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    DATABASE_NAME)
-                    .fallbackToDestructiveMigration()
-                    .build()
-            }
-            return INSTANCE
-        }
-
-        fun destroyInstance(){
-            INSTANCE =null
-        }
-    }
 }
