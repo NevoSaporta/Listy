a0a1dbf7984508bb9e2195e4df6cc9e7adcb93ed
NevoSaporta <nevosap@gmail.com>
2020-05-04 16:05:04 +0300

    Added dependency and modified db tables

--- app/src/main/java/com/nevosap/listy/database/AppDatabase.kt
+++ app/src/main/java/com/nevosap/listy/database/AppDatabase.kt
@@ -5,8 +5,9 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.nevosap.listy.model.GroceryItemModel
+import com.nevosap.listy.model.GroceryListModel
 
-@Database(entities = [GroceryItemModel::class,GroceryItemModel::class],version = 1)
+@Database(entities = [GroceryListModel::class,GroceryItemModel::class],version = 1,exportSchema = false)
 abstract class AppDatabase :RoomDatabase() {
 
     abstract fun listDao():GroceryListsDao
