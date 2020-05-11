c5d52aa964b144c1293cf7850358b548d3326c57
NevoSaporta <nevosap@gmail.com>
2020-05-11 14:06:13 +0300

    Fixing build errors

---
+++ app/src/main/java/com/nevosap/listy/database/Converters.kt
@@ -0,0 +1,26 @@
+package com.nevosap.listy.database
+
+import androidx.room.TypeConverter
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.nevosap.listy.model.GroceryItemOrderModel
+import java.util.*
+
+class Converters {
+    @TypeConverter
+    fun fromDateToLong(date: Date):Long{
+        return date.time
+    }
+    @TypeConverter
+    fun fromLongToDate(timeMills: Long):Date{
+        return Date(timeMills)
+    }
+    @TypeConverter
+    fun fromOrdersToString(orders:MutableList<GroceryItemOrderModel>) :String{
+        return Gson().toJson(orders)
+    }
+    @TypeConverter
+    fun fromStringToOrders(json:String):MutableList<GroceryItemOrderModel>{
+        return Gson().fromJson(json, object: TypeToken<MutableList<GroceryItemOrderModel>>(){}.type)
+    }
+}
