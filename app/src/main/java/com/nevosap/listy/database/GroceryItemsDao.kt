9bda82bb060d0ae54334040d7f67ecbcab0bd479
NevoSaporta <nevosap@gmail.com>
2020-05-11 14:24:25 +0300

    Added Dao annotation to grocery items dao

--- app/src/main/java/com/nevosap/listy/database/GroceryItemsDao.kt
+++ app/src/main/java/com/nevosap/listy/database/GroceryItemsDao.kt
@@ -1,10 +1,11 @@
 package com.nevosap.listy.database
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.nevosap.listy.model.GroceryItemModel
-
+@Dao
 interface GroceryItemsDao {
     @Query("SELECT * FROM GroceryItemModel")
     fun getItemsInStock():MutableList<GroceryItemModel>
