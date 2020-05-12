131717912bc78c63feeb22a79533c04f4c010725
Nevo Saporta <nevosap@gmail.com>
2020-05-12 20:51:09 +0300

    removed unnecessary file

--- app/src/main/java/com/nevosap/listy/edit/EditListClickListener.kt
+++
@@ -1,9 +0,0 @@
-package com.nevosap.listy.edit
-
-import com.nevosap.listy.databinding.ListItemGroceryEditBinding
-import com.nevosap.listy.model.GroceryItemModel
-import com.nevosap.listy.model.GroceryItemOrderModel
-
-class EditListClickListener (val clickListener: (MutableList<GroceryItemOrderModel>?, ListItemGroceryEditBinding, GroceryItemModel)->Unit){
-
-}
