84f62b9aee264fe24f8f60229cc38d1d10cbe669
NevoSaporta <nevosap@gmail.com>
2020-05-18 10:07:01 +0300

    Added sign up/in methods with firebase

--- app/src/main/java/com/nevosap/listy/users/UsersActivity.kt
+++ app/src/main/java/com/nevosap/listy/users/UsersActivity.kt
@@ -1,6 +1,26 @@
 package com.nevosap.listy.users
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.google.android.gms.auth.api.Auth
 
 class UsersActivity:AppCompatActivity() {
+    companion object {
+        private const val RC_SIGN_IN = 123
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startSignFlow()
+    }
+    private fun startSignFlow(){
+        val provider = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build())
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(provider)
+                .build(),
+            RC_SIGN_IN)
+    }
 }
