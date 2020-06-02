4d95a749f319e8bca76963bcca2cf904004c9129
NevoSaporta <nevosap@gmail.com>
2020-06-02 20:30:44 +0300

    Removed unnecessary drawer layout

--- app/src/main/java/com/nevosap/listy/MainActivity.kt
+++ app/src/main/java/com/nevosap/listy/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nevosap.listy
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
@@ -10,21 +11,11 @@ import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.google.firebase.auth.FirebaseAuth
-import com.nevosap.listy.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
-        val drawerLayout = binding.drawerLayout
-        val navController = this.findNavController(R.id.nav_host_fragment)
-        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
-        NavigationUI.setupWithNavController(binding.navigation, navController)
-    }
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = this.findNavController(R.id.nav_host_fragment)
-        return NavigationUI.navigateUp(navController,drawerLayout)
-    }
+        setContentView(R.layout.activity_main)
+}
 }
